b3cbfd3f5c93c145c5f5c52a0e6fbeafd67fe200
seiuneko <25706824+seiuneko@users.noreply.github.com>
2021-11-10 15:33:17 +0800

    Changed module cgi to html
    AttributeError: module 'cgi' has no attribute 'escape'

--- py3SimpleHTTPServerWithUpload.py
+++ py3SimpleHTTPServerWithUpload.py
@@ -34,7 +34,7 @@ import os
 import posixpath
 import http.server
 import urllib
-import cgi
+import html
 import shutil
 import mimetypes
 import re
@@ -251,7 +251,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 			self.send_error(404, "No permission to list directory")
 			return None
 		list.sort(key=lambda a: a.lower())
-		displaypath = cgi.escape(urllib.parse.unquote(self.path))
+		displaypath = html.escape(urllib.parse.unquote(self.path))
 
 		f = ('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">') +\
 			('<html><head>') +\
@@ -274,7 +274,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 			if os.path.islink(fullname):
 				displayname = name + "@"
 				# Note: a link to a directory displays with @ and links with /
-			f = f + ('<li><a href="%s">%s</a>' % (urllib.parse.quote(linkname), cgi.escape(displayname)))
+			f = f + ('<li><a href="%s">%s</a>' % (urllib.parse.quote(linkname), html.escape(displayname)))
 		f = f + ("</ul><hr></body></html>")
 
 		f = f.encode('utf-8')
